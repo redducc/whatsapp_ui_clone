53264b86dbd3c460d6c4667de9b1db776f5c0829
sushmit1 <sushmit718@gmail.com>
2022-11-06 21:38:21 +0530

    theme: Define app bar theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui_clone/whatsapp_home.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
     return MaterialApp(
       title: "WhatsApp",
       theme: ThemeData(
-          primaryColor: Color(0xff075E54), accentColor: Color(0xff25D366)),
+          primaryColor: Color(0xff075E54),
+          accentColor: Color(0xff25D366),
+          appBarTheme: AppBarTheme(color: Color(0xff075E54))),
       home: WhatsAppHome(),
     );
   }
