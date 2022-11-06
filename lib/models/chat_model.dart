5d69937855f790c6eb84f5675d339c7af56bac8e
sushmit1 <sushmit718@gmail.com>
2022-11-06 23:46:28 +0530

    clone: final changes

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,44 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+  ChatModel(
+      {required this.name,
+      required this.message,
+      required this.time,
+      required this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  ChatModel(
+      name: "resist15",
+      message: "bhai 404 banao",
+      time: "22:32",
+      avatarUrl: "https://avatars.githubusercontent.com/u/68729701?v=4"),
+  ChatModel(
+      name: "Reignz3",
+      message: "ninad patil rom katil",
+      time: "23:34",
+      avatarUrl: "https://avatars.githubusercontent.com/u/18658422?v=4"),
+  ChatModel(
+      name: "Bhasker",
+      message: "Mai pro hu",
+      time: "23:39",
+      avatarUrl: "https://avatars.githubusercontent.com/u/2985099?v=4"),
+  ChatModel(
+      name: "man of steel",
+      message: "real",
+      time: "23:69",
+      avatarUrl: "https://avatars.githubusercontent.com/u/69862555?v=4"),
+  ChatModel(
+      name: "Hades",
+      message: "Check my insta story",
+      time: "00:00",
+      avatarUrl: "https://avatars.githubusercontent.com/u/63295358?v=4"),
+  ChatModel(
+      name: "Sakthivel",
+      message: "nnn hai bhai",
+      time: "00:03",
+      avatarUrl: "https://avatars.githubusercontent.com/u/55133980?v=4")
+];
--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -2,15 +2,52 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui_clone/models/chat_model.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Chats",
-        style: TextStyle(fontSize: 20.0),
-      ),
+    return ListView.builder(
+      itemCount: dummyData.length,
+      itemBuilder: ((context, i) => Column(
+            children: <Widget>[
+              Divider(
+                height: 10.0,
+              ),
+              ListTile(
+                leading: CircleAvatar(
+                  foregroundColor: Theme.of(context).primaryColor,
+                  backgroundColor: Colors.grey,
+                  backgroundImage: NetworkImage(dummyData[i].avatarUrl),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      dummyData[i].name,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      dummyData[i].time,
+                      style: TextStyle(color: Colors.grey, fontSize: 14.0),
+                    ),
+                  ],
+                ),
+                subtitle: Container(
+                  padding: const EdgeInsets.only(top: 5.0),
+                  child: Text(
+                    dummyData[i].message,
+                    style: TextStyle(color: Colors.grey, fontSize: 15.0),
+                  ),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
